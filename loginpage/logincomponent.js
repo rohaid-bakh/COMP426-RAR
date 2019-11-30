39f864c1bb7c9e4240ce27c79f45e83c07689dab
afanfic <you@example.com>
2019-11-29 21:34:44 -0500

    edited index.html/ added axiosfunction logincomonent.js

--- loginpage/logincomponent.js
+++ loginpage/logincomponent.js
@@ -1,1 +1,89 @@
-alert("hello World");
+// import axios from '../node_modules/axios/dist/axios.js';
+const pubRoot = new axios.create({
+  baseURL: "http://localhost:3000/public"
+});
+
+// {
+//     "name": "chris",
+//     "pass": "pass123",
+//     "data": {
+//       "role": 2,
+//       "description": "Lazy..."
+//     }
+//   }
+
+$(function() {
+    $("input.button.is-success.is-inverted").on("click" , event, function(){
+        event.preventDefault();
+        handleLogin(event);
+    });
+    function handleLogin(event){
+        console.log(event);
+        alert("event");
+        console.log(document.getElementById("un").value);
+        console.log(document.getElementById("pw").value);
+        console.log(checkLogin());
+       
+    }
+    async function checkLogin() {
+        return await pubRoot.post(`/login`, {
+          name: document.getElementById("un").value,
+          pass: document.getElementById("pw").value,
+          data: {},
+        })
+      }
+
+    // $('#test').click(test);
+    // function test(event){
+    //   event.preventDefault();
+    //   console.log('hi');
+    //   const pubRoot = new axios.create({
+    //     baseURL: "http://localhost:3000/public"
+    //   });
+      
+    //   async function createAuthor({first = 'John', last = 'Doe', numBooks = 0}) {
+    //     return await pubRoot.post(`/authors/`, {
+    //       data: {first, last, numBooks}
+    //     })
+    //   }
+      
+    //   async function getAllAuthors() {
+    //     return await pubRoot.get('/authors');
+    //   }
+      
+    //   (async () => {
+    //     await createAuthor({
+    //       first: "chris",
+    //       numBooks: 4
+    //     });
+      
+    //     let {data} = await getAllAuthors();
+    //     console.log(data)
+    //   })();
+        
+    // }
+  });
+
+// async function createAuthor({first = 'John', last = 'Doe', numBooks = 0}) {
+//   return await pubRoot.post(`/authors/`, {
+//     data: {first, last, numBooks}
+//   })
+// }
+
+// async function getAllAuthors() {
+//   return await pubRoot.get('/authors');
+// }
+
+// (async () => {
+//   await createAuthor({
+//     first: "chris",
+//     numBooks: 4
+//   });
+
+//   let {data} = await getAllAuthors();
+//   console.log(data)
+// })();
+
+// async function handleLogin(){
+//     $("input.button.is-success.is-inverted").on("click",)
+// }
--- render.js
+++ render.js
@@ -1,48 +1,48 @@
-export const loadArrayIntoDOM = function() {
-    $(document).on("click");
+// export const loadArrayIntoDOM = function() {
+//     $(document).on("click");
 
-    $(document).click(event => {
-        handleButtonPress(event, experiment);
-        event.preventDefault();
-    });
-    $root.on("click", "button#id", function() {
-        experiment.setupNewGame();
-        let z = renderCurrentArray(experiment.gameState);
-        $("h3#score").replaceWith(
-            "<h3 id='score'>Score:" + experiment.gameState.score + "</h3>"
-        );
-        $("h3#lose").remove();
-        $("h3#win").remove();
-        $("section#board").replaceWith(z);
-    });
-};
+//     $(document).click(event => {
+//         handleButtonPress(event, experiment);
+//         event.preventDefault();
+//     });
+//     $root.on("click", "button#id", function() {
+//         experiment.setupNewGame();
+//         let z = renderCurrentArray(experiment.gameState);
+//         $("h3#score").replaceWith(
+//             "<h3 id='score'>Score:" + experiment.gameState.score + "</h3>"
+//         );
+//         $("h3#lose").remove();
+//         $("h3#win").remove();
+//         $("section#board").replaceWith(z);
+//     });
+// };
 
-$(function() {
-    loadArrayIntoDOM();
-});
+// $(function() {
+//     loadArrayIntoDOM();
+// });
 
-import axios from 'axios';
+// import axios from 'axios';
 
-const pubRoot = new axios.create({
-  baseURL: "http://localhost:3000/public"
-});
+// const pubRoot = new axios.create({
+//   baseURL: "http://localhost:3000/public"
+// });
 
-async function createAuthor({first = 'John', last = 'Doe', numBooks = 0}) {
-  return await pubRoot.post(`/authors/`, {
-    data: {first, last, numBooks}
-  })
-}
+// async function createAuthor({first = 'John', last = 'Doe', numBooks = 0}) {
+//   return await pubRoot.post(`/authors/`, {
+//     data: {first, last, numBooks}
+//   })
+// }
 
-async function getAllAuthors() {
-  return await pubRoot.get('/authors');
-}
+// async function getAllAuthors() {
+//   return await pubRoot.get('/authors');
+// }
 
-(async () => {
-  await createAuthor({
-    first: "chris",
-    numBooks: 4
-  });
+// (async () => {
+//   await createAuthor({
+//     first: "chris",
+//     numBooks: 4
+//   });
 
-  let {data} = await getAllAuthors();
-  console.log(data)
-})();
+//   let {data} = await getAllAuthors();
+//   console.log(data)
+// })();
